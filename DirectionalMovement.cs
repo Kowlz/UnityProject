85337629d5d56b1b3b61c504baafdacef8442e3c
Kowlz <mickael-marlard@hotmail.com>
2016-11-18 16:38:27 +0100

    Add files via upload

---
+++ DirectionalMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectionalMovement : MonoBehaviour {
+
+	public int speed;
+	public int direction;
+
+	void Update () {
+		Move();
+	}
+
+	void Move() {
+        float temp = direction * speed * Time.deltaTime;
+        Vector3 newPos = transform.position;
+
+        newPos += new Vector3(temp, 0, 0);
+        transform.position = newPos;
+
+	}
+}
---
+++ IADirectionalMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IADirectionalMovement : MonoBehaviour {
+
+	public int speed;
+	public int direction;
+    public LayerMask layer;
+
+	void Update () {
+		Move();
+	}
+
+	void Move() {
+        float temp = direction * speed * Time.deltaTime;
+        Vector3 newPos = transform.position;
+
+        newPos += new Vector3(temp, 0, 0);
+
+        Vector3 testerPos = new Vector3(0,0,0);
+
+        switch (direction)
+        {         
+            case -1:
+                testerPos = transform.Find("LeftTester").transform.position;
+                break;
+            case 1:
+                testerPos = transform.Find("RightTester").transform.position;         
+                break;
+            default:
+                break;
+        }
+
+        if (Physics2D.OverlapPoint(testerPos,layer))
+        {
+            transform.position = newPos;
+        }
+        else
+        {
+            direction *= -1;
+        }
+
+	}
+}
